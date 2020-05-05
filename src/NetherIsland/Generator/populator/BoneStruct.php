415c3b44dfe2bbd02eaa88626077ce0df6e94064
IllegalerKater <mcpe.cathesda@gmail.com>
2020-05-05 18:27:23 +0200

    Fix?

--- src/NetherIsland/Generator/populator/BoneStruct.php
+++ src/NetherIsland/Generator/populator/BoneStruct.php
@@ -1,5 +1,6 @@
 <?php
 
+
 declare(strict_types=1);
 
 namespace NetherIsland\Generator\populator;
@@ -16,6 +17,11 @@ class BoneStruct extends Populator {
     private $randomAmount;
     private $baseAmount;
 
+    public function __construct(int $randomAmount = 0, int $baseAmount = 0){
+        $this->baseAmount = $baseAmount;
+        $this->randomAmount = $randomAmount;
+    }
+
     public function setRandomAmount($amount) {
         $this->randomAmount = $amount;
     }
@@ -24,40 +30,56 @@ class BoneStruct extends Populator {
         $this->baseAmount = $amount;
     }
 
+    /**
+     * @param ChunkManager $level
+     * @param int $chunkX
+     * @param int $chunkZ
+     * @param Random $random
+     * @return mixed|void
+     */
     public function populate(ChunkManager $level, int $chunkX, int $chunkZ, Random $random) {
-        if (mt_rand(0, 100) < 10) {
+        if(mt_rand(0, 99) < 10) {
             $this->level = $level;
             $amount = $random->nextRange(0, $this->randomAmount + 1) + $this->baseAmount;
             for ($i = 0; $i < $amount; ++$i) {
                 $x = $random->nextRange($chunkX * 16, $chunkX * 16 + 15);
                 $z = $random->nextRange($chunkZ * 16, $chunkZ * 16 + 15);
-                $y = $this->getHighestWorkableBlock($x, $z);
+                $y = $random->nextRange(0, 120);
                 if ($this->level->getBlockIdAt($x, $y, $z) == Block::NETHERRACK) {
-                    for($nz = $z; $nz < $z + 6; $nz + 2) {
-                        $level->setBlockIdAt($x, $y, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x - 1, $y, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x + 1, $y, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x - 2, $y + 1, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x + 2, $y + 1, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x - 3, $y + 2, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x - 3, $y + 2, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x - 3, $y + 3, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x - 3, $y + 3, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x - 2, $y + 4, $nz, Block::BONE_BLOCK);
-                        $level->setBlockIdAt($x - 2, $y + 4, $nz, Block::BONE_BLOCK);
-                    }
+                    $this->spawnRibStruct($level, $x, $y, $z);
                 }
             }
         }
     }
 
-    private function getHighestWorkableBlock($x, $z) {
-        for ($y = 127; $y >= 0; --$y) {
-            $b = $this->level->getBlockIdAt($x, $y, $z);
-            if ($b == Block::NETHERRACK) {
-                break;
-            }
+    public static function spawnBone(ChunkManager $level, $x, $y, $z) {
+        // Todo if even working
+    }
+
+    public static function spawnRibStruct(ChunkManager $level, $x, $y, $z){
+        for($nz = $z; $nz < $z + 6; $nz + 2) {
+            $level->setBlockIdAt($x, $y, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x - 1, $y, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x + 1, $y, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x - 2, $y + 1, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x + 2, $y + 1, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x - 3, $y + 2, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x - 3, $y + 2, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x - 3, $y + 3, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x - 3, $y + 3, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x - 2, $y + 4, $nz, Block::BONE_BLOCK);
+            $level->setBlockIdAt($x - 2, $y + 4, $nz, Block::BONE_BLOCK);
+            $level->setBlockDataAt($x, $y, $nz, 0);
+            $level->setBlockDataAt($x - 1, $y, $nz, 0);
+            $level->setBlockDataAt($x + 1, $y, $nz, 0);
+            $level->setBlockDataAt($x - 2, $y + 1, $nz, 0);
+            $level->setBlockDataAt($x + 2, $y + 1, $nz, 0);
+            $level->setBlockDataAt($x - 3, $y + 2, $nz, 0);
+            $level->setBlockDataAt($x - 3, $y + 2, $nz, 0);
+            $level->setBlockDataAt($x - 3, $y + 3, $nz, 0);
+            $level->setBlockDataAt($x - 3, $y + 3, $nz, 0);
+            $level->setBlockDataAt($x - 2, $y + 4, $nz, 0);
+            $level->setBlockDataAt($x - 2, $y + 4, $nz, 0);
         }
-        return $y === 0 ? -1 : $y;
     }
 }
